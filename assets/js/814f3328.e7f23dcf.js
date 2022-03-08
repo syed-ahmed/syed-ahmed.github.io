466601f633ae064404a2da04d066537ac57805dd
syed-ahmed <syed-ahmed@users.noreply.github.com>
2022-03-08 19:43:58 +0000

    deploy: b7b500aee3774598c422532d11fa9ba3b3c2bf56

---
+++ assets/js/814f3328.e7f23dcf.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[2535],{5641:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Creating Vivado Hardware Platform using YAML","permalink":"/blog/yaml-based-vivado-hardware-project"},{"title":"Use All Tunable Vivado Internal Params","permalink":"/blog/vivado-internal-params"}]}')}}]);
