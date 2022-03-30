8f5b3e8df3f911f88c1c064eecc0f498949330fa
Syed Tousif Ahmed <stahmed@seas.upenn.edu>
2022-03-29 23:16:20 -0400

    Add hidebar config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -43,6 +43,8 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      autoCollapseSidebarCategories: true,
+      hideableSidebar: true,
       navbar: {
         title: 'Syed Tousif Ahmed',
         items: [
