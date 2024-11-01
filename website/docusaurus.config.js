e1811ecd063d4bf089df6a9d15ccf3d58ac899c3
Syed Tousif Ahmed <syed.ahmed.emails@gmail.com>
2024-10-31 23:53:36 -0700

    Update docusaurus.config.js

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -44,7 +44,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      hideableSidebar: true,
+      docs: {
+        sidebar: {
+          hideable: true,
+        },
+      },
       navbar: {
         title: 'Syed Tousif Ahmed',
         items: [
